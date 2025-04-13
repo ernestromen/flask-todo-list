ec7c4ac61953ca717dd1e69d141cba833c47327b
ernestromen <ernestromen1996@gmail.com>
2025-04-13 15:24:11 +0300

    Started building frontend and as of now creation of users table does not work

--- api/app.py
+++ api/app.py
@@ -1,7 +1,39 @@
+import os
+import psycopg2
 from flask import Flask, jsonify, make_response
+
 
 app = Flask(__name__)
 
+def get_db_connection():
+    con = psycopg2.connect(host='db',database='task_management',user=os.environ['DB_USERNAME'],password=os.environ['DB_PASSWORD'])
+    return con
+ 
+
+@app.route('/users')
+ 
+def index():
+    # return "Hello from /users!"
+
+    conn = get_db_connection()
+ 
+
+    cur = conn.cursor()
+ 
+
+    cur.execute('SELECT * FROM users;')
+ 
+
+    books = cur.fetchall()
+ 
+
+    cur.close()
+ 
+
+    conn.close()
+ 
+
+    return render_template('index.html', books=books)
 
 @app.route('/api/v1.0/test', methods=['GET'])
 def test_response():
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 class App extends React.Component {
   constructor(props) {
@@ -14,7 +15,7 @@ class App extends React.Component {
 
   componentDidMount() {
     fetch("http://localhost:5000/api/v1.0/test")
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(
         (result) => {
           this.setState({
@@ -28,31 +29,42 @@ class App extends React.Component {
             error,
           });
         }
-      )
+      );
   }
 
   render() {
-    const {error, isLoaded, items} = this.state;
+    const { error, isLoaded, items } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
       return (
-        <ul>
-          {items.map(item => (
-            <li key={item.name}>
-              {item.name} {item.price}
-            </li>
-          ))}
-        </ul>
+        <body>
+          <div class="header">
+            <h1>Header</h1>
+            <p>My supercool header</p>
+          </div>
+          <div class="sidebar">
+            <a class="active" href="#home">
+              Home
+            </a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+          </div>
+          <button className="btn btn-success">testing bootstrap</button>
+          <ul>
+            {items.map((item) => (
+              <li key={item.name}>
+                {item.name} {item.price}
+              </li>
+            ))}
+          </ul>
+        </body>
       );
     }
   }
 }
 
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
